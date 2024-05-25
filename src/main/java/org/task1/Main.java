6def3014f52a300b505976551a44542b0be5e1ad
dekaloll <metra.adsky@yandex.ru>
2024-05-25 22:22:45 +0300

    Initial commit

---
+++ src/main/java/org/task1/Main.java
@@ -0,0 +1,30 @@
+package org.task1;
+
+import ru.pflb.mq.dummy.exception.DummyException;
+import ru.pflb.mq.dummy.implementation.ConnectionImpl;
+import ru.pflb.mq.dummy.implementation.DestinationImpl;
+import ru.pflb.mq.dummy.implementation.ProducerImpl;
+import ru.pflb.mq.dummy.implementation.SessionImpl;
+
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, DummyException {
+        ConnectionImpl conn = new ConnectionImpl();
+        SessionImpl sess = (SessionImpl) conn.createSession(true);
+        DestinationImpl dest = (DestinationImpl) sess.createDestination("Поток пошел");
+        ProducerImpl prod = (ProducerImpl) sess.createProducer(dest);
+
+        ArrayList<String> myList = new ArrayList<>();
+        myList.add("Четыре");
+        myList.add("Пять");
+        myList.add("Шесть");
+
+        for(String s : myList) {
+            prod.send(s);
+            Thread.sleep(2000);
+        }
+        conn.close();
+    }
+}
